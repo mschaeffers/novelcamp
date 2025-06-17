bf8b379e0ee9d79d0b27400bc8c5acb93c111427
Michael Schaeffers <michael.schaeffers@crv4all.co.nz>
2025-06-17 20:28:51 +1200

    Login

---
+++ app/Pages/Login.php
@@ -0,0 +1,12 @@
+<html>
+
+<head>
+    <title>Login Page</title>
+    <link rel="stylesheet" type="text/css" href="../styles.css">
+</head>
+
+<body>
+    <p><a href="../index.php">Home</a></p>
+    <h1>Login</h1>
+
+</html>
